ddb1e9a9773bcbb7f6fe9cfde40d7643cb1db233
syafiqm7 <39830823+syafiqm7@users.noreply.github.com>
2018-07-26 18:39:44 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+os.system('pkg install libcaca')
+os.system('pkg install ruby')
+os.system('gem install lolcat')
+os.system('pkg install moon-buggy')
+os.system('pkg install figlet' )
+os.system('pkg install cmatrix')
+os.system('figlet DONE! |lolcat')
+os.system('echo Please type python log.py to execute the program. | lolcat')
