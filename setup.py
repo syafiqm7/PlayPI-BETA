9147594695e042ddf0c9920d22682eea18698f64
syafiqm7 <39830823+syafiqm7@users.noreply.github.com>
2018-07-26 19:17:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-os.system('pkg install libcaca')
-os.system('pkg install ruby')
+os.system('apt-get install libcaca')
+os.system('apt-get install ruby')
 os.system('gem install lolcat')
-os.system('pkg install moon-buggy')
-os.system('pkg install figlet' )
-os.system('pkg install cmatrix')
+os.system('apt-get install moon-buggy')
+os.system('apt-get install figlet' )
+os.system('apt-get install cmatrix')
 os.system('figlet DONE! |lolcat')
 os.system('echo Please type python play.py to execute the program. | lolcat')
